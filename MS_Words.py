8b2c77da2cedf5b0318e43a71874554a4c824951
Шанин Андрей <hedgestandart@gmail.com>
2023-11-06 22:09:16 +0300

    done

--- MS_Words.py
+++ MS_Words.py
@@ -1,8 +1,60 @@
-from fastapi import FastAPI
+import json
 
-app = FastAPI()
+import jsonpickle
+from flask import Flask
+from flask import render_template
+from flask import request
+from json  import decoder
+import jsonpickle
+
+app = Flask(__name__)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class Words_stat:
+    text = ""
+    words = []
+    count = 0
+
+    def __init__(self, text):
+        self.text = text
+        self.words = self.text.split(' ')
+        self.count = len(self.words)
+
+    def default(self, o):
+        return o.__dict__
+
+    def  json(self):
+        return jsonpickle.encode(self)
+
+##=== = = = = Главная программа  === == =
+@app.route("/")
+def index():
+    return "<html> <head> </head> <body lang=ru> <p> Привет!  Введите строку <form name=words method=post action='/words'> <input name='words' type=text></input> <input type=submit text='Ok'> </form></p>  </body> </html>"
+
+@app.route("/words", methods = ['POST'])
+def index2():
+    words = request.form['words']
+    data = []
+
+    try:
+        dec = json.loads( words)
+        print ("wait data = ", dec ["data"] )
+        obj = Words_stat(dec["data"])
+
+        print("obj = ", obj)
+        data= obj.json()
+
+    except:
+
+        obj = Words_stat(words)
+        print("obj = ", obj)
+
+        data = obj.json()
+
+    return data
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
+
+
